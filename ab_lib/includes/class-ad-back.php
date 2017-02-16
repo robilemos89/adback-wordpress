860c1edfe9c84c0ef28e68372a14cd622240e48a
Julien Guilmont <julien@dekalee.fr>
2017-02-16 16:53:27 +0100

    Modify indentation to 4 spaces

--- ab_lib/includes/class-ad-back.php
+++ ab_lib/includes/class-ad-back.php
@@ -29,195 +29,195 @@
  */
 class Ad_Back
 {
-	/**
-	 * The loader that's responsible for maintaining and registering all hooks that power
-	 * the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   protected
-	 * @var      Ad_Back_Loader    $loader    Maintains and registers all hooks for the plugin.
-	 */
-	protected $loader;
+    /**
+     * The loader that's responsible for maintaining and registering all hooks that power
+     * the plugin.
+     *
+     * @since    1.0.0
+     * @access   protected
+     * @var      Ad_Back_Loader    $loader    Maintains and registers all hooks for the plugin.
+     */
+    protected $loader;
 
-	/**
-	 * The unique identifier of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   protected
-	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
-	 */
-	protected $plugin_name;
+    /**
+     * The unique identifier of this plugin.
+     *
+     * @since    1.0.0
+     * @access   protected
+     * @var      string    $plugin_name    The string used to uniquely identify this plugin.
+     */
+    protected $plugin_name;
 
-	/**
-	 * The current version of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   protected
-	 * @var      string    $version    The current version of the plugin.
-	 */
-	protected $version;
+    /**
+     * The current version of the plugin.
+     *
+     * @since    1.0.0
+     * @access   protected
+     * @var      string    $version    The current version of the plugin.
+     */
+    protected $version;
 
-	/**
-	 * Define the core functionality of the plugin.
-	 *
-	 * Set the plugin name and the plugin version that can be used throughout the plugin.
-	 * Load the dependencies, define the locale, and set the hooks for the admin area and
-	 * the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function __construct()
+    /**
+     * Define the core functionality of the plugin.
+     *
+     * Set the plugin name and the plugin version that can be used throughout the plugin.
+     * Load the dependencies, define the locale, and set the hooks for the admin area and
+     * the public-facing side of the site.
+     *
+     * @since    1.0.0
+     */
+    public function __construct()
     {
-		$this->plugin_name = 'ab-lib';
-		$this->version = '1.0.0';
+        $this->plugin_name = 'ab-lib';
+        $this->version = '1.0.0';
 
-		$this->loadDependencies();
-		$this->setLocale();
-		$this->defineAdminHooks();
-		$this->definePublicHooks();
-	}
+        $this->loadDependencies();
+        $this->setLocale();
+        $this->defineAdminHooks();
+        $this->definePublicHooks();
+    }
 
-	/**
-	 * Load the required dependencies for this plugin.
-	 *
-	 * Include the following files that make up the plugin:
-	 *
-	 * - Ad_Back_Loader. Orchestrates the hooks of the plugin.
-	 * - Ad_Back_i18n. Defines internationalization functionality.
-	 * - Ad_Back_Admin. Defines all hooks for the admin area.
-	 * - Ad_Back_Public. Defines all hooks for the public side of the site.
-	 *
-	 * Create an instance of the loader which will be used to register the hooks
-	 * with WordPress.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function loadDependencies()
+    /**
+     * Load the required dependencies for this plugin.
+     *
+     * Include the following files that make up the plugin:
+     *
+     * - Ad_Back_Loader. Orchestrates the hooks of the plugin.
+     * - Ad_Back_i18n. Defines internationalization functionality.
+     * - Ad_Back_Admin. Defines all hooks for the admin area.
+     * - Ad_Back_Public. Defines all hooks for the public side of the site.
+     *
+     * Create an instance of the loader which will be used to register the hooks
+     * with WordPress.
+     *
+     * @since    1.0.0
+     * @access   private
+     */
+    private function loadDependencies()
     {
-		/**
-		 * The class responsible for orchestrating the actions and filters of the
-		 * core plugin.
-		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-ad-back-loader.php';
+        /**
+         * The class responsible for orchestrating the actions and filters of the
+         * core plugin.
+         */
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-ad-back-loader.php';
 
-		/**
-		 * The class responsible for defining internationalization functionality
-		 * of the plugin.
-		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-ad-back-i18n.php';
+        /**
+         * The class responsible for defining internationalization functionality
+         * of the plugin.
+         */
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-ad-back-i18n.php';
 
-		/**
-		 * The class responsible for defining all actions that occur in the admin area.
-		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-ad-back-admin.php';
+        /**
+         * The class responsible for defining all actions that occur in the admin area.
+         */
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-ad-back-admin.php';
 
-		/**
-		 * The class responsible for defining all actions that occur in the public-facing
-		 * side of the site.
-		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-ad-back-public.php';
+        /**
+         * The class responsible for defining all actions that occur in the public-facing
+         * side of the site.
+         */
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-ad-back-public.php';
 
-		$this->loader = new Ad_Back_Loader();
-	}
+        $this->loader = new Ad_Back_Loader();
+    }
 
-	/**
-	 * Define the locale for this plugin for internationalization.
-	 *
-	 * Uses the Ad_Back_i18n class in order to set the domain and to register the hook
-	 * with WordPress.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function setLocale()
+    /**
+     * Define the locale for this plugin for internationalization.
+     *
+     * Uses the Ad_Back_i18n class in order to set the domain and to register the hook
+     * with WordPress.
+     *
+     * @since    1.0.0
+     * @access   private
+     */
+    private function setLocale()
     {
-		$plugin_i18n = new Ad_Back_i18n();
+        $plugin_i18n = new Ad_Back_i18n();
 
-		$this->loader->addAction( 'plugins_loaded', $plugin_i18n, 'loadPluginTextdomain' );
-	}
+        $this->loader->addAction( 'plugins_loaded', $plugin_i18n, 'loadPluginTextdomain' );
+    }
 
-	/**
-	 * Register all of the hooks related to the admin area functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function defineAdminHooks()
+    /**
+     * Register all of the hooks related to the admin area functionality
+     * of the plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     */
+    private function defineAdminHooks()
     {
-		$plugin_admin = new Ad_Back_Admin( $this->getPluginName(), $this->getVersion() );
+        $plugin_admin = new Ad_Back_Admin( $this->getPluginName(), $this->getVersion() );
 
-		$this->loader->addAction( 'admin_enqueue_scripts', $plugin_admin, 'enqueueStyles' );
-		$this->loader->addAction( 'admin_enqueue_scripts', $plugin_admin, 'enqueueScripts' );
+        $this->loader->addAction( 'admin_enqueue_scripts', $plugin_admin, 'enqueueStyles' );
+        $this->loader->addAction( 'admin_enqueue_scripts', $plugin_admin, 'enqueueScripts' );
 
-		// Add menu item
-		$this->loader->addAction( 'admin_menu', $plugin_admin, 'addPluginAdminMenu' );
+        // Add menu item
+        $this->loader->addAction( 'admin_menu', $plugin_admin, 'addPluginAdminMenu' );
 
 
-		// Add Ajax
-		$this->loader->addAction( 'wp_ajax_callApi', $plugin_admin,'callApiCallback' );
-		$this->loader->addAction( 'wp_ajax_registerWithAbBackAccount', $plugin_admin, 'registerWithAbBackAccountCallback');
-		$this->loader->addAction( 'wp_ajax_ab_logout', $plugin_admin, 'logoutCallback');
-		$this->loader->addAction( 'wp_ajax_saveMessage', $plugin_admin, 'saveMessageCallback');
-	}
+        // Add Ajax
+        $this->loader->addAction( 'wp_ajax_callApi', $plugin_admin,'callApiCallback' );
+        $this->loader->addAction( 'wp_ajax_registerWithAbBackAccount', $plugin_admin, 'registerWithAbBackAccountCallback');
+        $this->loader->addAction( 'wp_ajax_ab_logout', $plugin_admin, 'logoutCallback');
+        $this->loader->addAction( 'wp_ajax_saveMessage', $plugin_admin, 'saveMessageCallback');
+    }
 
-	/**
-	 * Register all of the hooks related to the public-facing functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function definePublicHooks()
+    /**
+     * Register all of the hooks related to the public-facing functionality
+     * of the plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     */
+    private function definePublicHooks()
     {
-		$plugin_public = new Ad_Back_Public( $this->getPluginName(), $this->getVersion() );
+        $plugin_public = new Ad_Back_Public( $this->getPluginName(), $this->getVersion() );
 
-		$this->loader->addAction( 'wp_enqueue_scripts', $plugin_public, 'enqueueStyles' );
-		$this->loader->addAction( 'wp_enqueue_scripts', $plugin_public, 'enqueueScripts' );
-	}
+        $this->loader->addAction( 'wp_enqueue_scripts', $plugin_public, 'enqueueStyles' );
+        $this->loader->addAction( 'wp_enqueue_scripts', $plugin_public, 'enqueueScripts' );
+    }
 
-	/**
-	 * Run the loader to execute all of the hooks with WordPress.
-	 *
-	 * @since    1.0.0
-	 */
-	public function run()
+    /**
+     * Run the loader to execute all of the hooks with WordPress.
+     *
+     * @since    1.0.0
+     */
+    public function run()
     {
-		$this->loader->run();
-	}
+        $this->loader->run();
+    }
 
-	/**
-	 * The name of the plugin used to uniquely identify it within the context of
-	 * WordPress and to define internationalization functionality.
-	 *
-	 * @since     1.0.0
-	 * @return    string    The name of the plugin.
-	 */
-	public function getPluginName()
+    /**
+     * The name of the plugin used to uniquely identify it within the context of
+     * WordPress and to define internationalization functionality.
+     *
+     * @since     1.0.0
+     * @return    string    The name of the plugin.
+     */
+    public function getPluginName()
     {
-		return $this->plugin_name;
-	}
+        return $this->plugin_name;
+    }
 
-	/**
-	 * The reference to the class that orchestrates the hooks with the plugin.
-	 *
-	 * @since     1.0.0
-	 * @return    Ad_Back_Loader    Orchestrates the hooks of the plugin.
-	 */
-	public function getLoader()
+    /**
+     * The reference to the class that orchestrates the hooks with the plugin.
+     *
+     * @since     1.0.0
+     * @return    Ad_Back_Loader    Orchestrates the hooks of the plugin.
+     */
+    public function getLoader()
     {
-		return $this->loader;
-	}
+        return $this->loader;
+    }
 
-	/**
-	 * Retrieve the version number of the plugin.
-	 *
-	 * @since     1.0.0
-	 * @return    string    The version number of the plugin.
-	 */
-	public function getVersion()
+    /**
+     * Retrieve the version number of the plugin.
+     *
+     * @since     1.0.0
+     * @return    string    The version number of the plugin.
+     */
+    public function getVersion()
     {
-		return $this->version;
-	}
+        return $this->version;
+    }
 }
