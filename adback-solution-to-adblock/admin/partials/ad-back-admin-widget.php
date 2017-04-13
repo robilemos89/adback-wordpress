319f162fd87a6a9c535183733a98b17eb1cd820d
Julien Guilmont <julien@dekalee.fr>
2017-04-13 11:36:12 +0200

    Add table for widget + update translations

--- adback-solution-to-adblock/admin/partials/ad-back-admin-widget.php
+++ adback-solution-to-adblock/admin/partials/ad-back-admin-widget.php
@@ -12,15 +12,39 @@
  * @subpackage Ad_Back/admin/partials
  */
 ?>
+<h2><?php _e('Yesterday', 'ad-back'); ?></h2>
+<br/>
+<table id="table-page-view" data-ab-table data-ab-type="yesterday-table-page-views" style="display: none;">
+    <thead>
+    <tr>
+        <th class="name"></th>
+        <th class="number"><?php _e( 'Number', 'ad-back'); ?></th>
+        <th class="rate">%</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr class="sample">
+        <td class="name"></td>
+        <td class="number"></td>
+        <td class="rate"></td>
+    </tr>
+    </tbody>
+</table>
+<br/>
 <h2><?php _e('Last 7 days', 'ad-back'); ?></h2>
 <br/>
+<h3><?php _e( 'Adblock removals', 'ad-back' ); ?></h3>
+<p><?php _e( '% adblock removals after showing message', 'ad-back' ); ?></p>
+<div data-ab-graph data-ab-type="last-7-adblocker-rate" data-ab-no-period="1" data-ab-no-data="<?php printf(esc_attr('No custom message enabled. You can enable it <a href="%s">here</a>', 'ad-back'), get_admin_url(null, 'admin.php?page=ab-settings') ); ?>" style="width: 95%; height: 200px; margin-bottom: 10px;">
+</div>
+<hr/>
 <h3><?php _e( 'Blocked page view and percent', 'ad-back' ); ?></h3>
-<div data-ab-graph data-ab-type="yesterday-page-view-adblocker-percent" data-ab-no-period="1" style="width: 95%; height: 400px; margin-bottom: 50px;">
+<div data-ab-graph data-ab-type="last-7-page-view-adblocker-percent" data-ab-no-period="1" style="width: 95%; height: 400px; margin-bottom: 50px;">
 </div>
 
 <script type="text/javascript">
-	window.onload = function() {
-		if(typeof adbackjs === 'object') {
+    window.onload = function() {
+        if(typeof adbackjs === 'object') {
             adbackjs.init({
                 token: '<?php echo $this->getToken()->access_token; ?>',
                 url: 'https://<?php echo $this->getDomain(); ?>/api/',
@@ -29,3 +53,28 @@
         }
     }
 </script>
+<style>
+    #table-page-view {
+        border-collapse: collapse;
+    }
+    #table-page-view .sample {
+        display:none;
+    }
+    #table-page-view td {
+        border:1px solid #e5e5e5;
+        margin: 0;
+    }
+    #table-page-view td, #table-page-view th{
+        padding: 10px;
+    }
+    #table-page-view thead{
+        background-color: #f1f1f1;
+    }
+    #table-page-view th.number, #table-page-view th.rate{
+        border-left: 1px solid #e5e5e5;
+        border-right: 1px solid #e5e5e5;
+    }
+    #table-page-view .rate{
+        min-width: 50px;
+    }
+</style>
