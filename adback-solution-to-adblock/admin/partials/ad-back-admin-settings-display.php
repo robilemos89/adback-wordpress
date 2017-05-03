9f62e31a7fd7edace7aa624aa1fbd571e2108584
Nicolas Thal <nico.th4l@gmail.com>
2017-05-03 15:43:45 +0200

    display configuration form in settings page

--- adback-solution-to-adblock/admin/partials/ad-back-admin-settings-display.php
+++ adback-solution-to-adblock/admin/partials/ad-back-admin-settings-display.php
@@ -16,29 +16,23 @@
 <?php include "ad-back-admin-header.php" ?>
 <h1 class="ab-h1"><?php _e( 'AdBack Settings', 'ad-back' ); ?></h1>
 <div id="ab-settings">
-    <h3><?php _e('Display message', 'ad-back'); ?></h3>
-    <fieldset>
-        <legend class="screen-reader-text">
-            <span><?php esc_html_e('Activate your message', 'ad-back'); ?></span>
-        </legend>
-        <label for="ab-settings-display">
-            <input type="checkbox" id="ab-settings-display" value="1" <?php echo ($messages['display'] == '1' ? "checked='checked'" : ""); ?>> <?php esc_html_e('Activate your message', 'ad-back'); ?>
-        </label>
-    </fieldset>
-<br/>
-    <fieldset>
-        <legend class="screen-reader-text">
-            <span><?php esc_html_e('No message for logged in Wordpress admin', 'ad-back'); ?></span>
-        </legend>
-        <label for="ab-settings-hide-admin">
-            <input type="checkbox" id="ab-settings-hide-admin" value="0" <?php echo (get_option('adback_admin_hide_message', '1') === '1' ? "checked='checked'" : ""); ?>> <?php esc_html_e('No message for logged in Wordpress admin', 'ad-back'); ?>
-        </label>
-    </fieldset>
-	<p class="submit">
-		<input type="submit" id="ab-settings-submit" class="button button-primary" value="<?php esc_html_e('Save', 'ad-back'); ?>">
-	</p>
+    <div id="ab-full-app">
+        <div id="ab-configuration-form"></div>
+    </div>
 </div>
 <div class="ab-primary-setting">
     <h3><?php esc_html_e('Adback Account', 'ad-back'); ?></h3>
     <button id="ab-logout" class="button button-primary"><?php esc_html_e('Log out', 'ad-back'); ?></button>
 </div>
+
+<script type="text/javascript">
+    window.onload = function () {
+        if (typeof adbackjs === 'object') {
+            adbackjs.init({
+                token: '<?php echo $this->getToken()->access_token; ?>',
+                url: 'https://<?php echo $this->getDomain(); ?>/api/',
+                language: '<?php echo str_replace('_', '-', get_locale()); ?>'
+            });
+        }
+    }
+</script>
