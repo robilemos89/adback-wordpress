b7e5288998546550089aa0af3f0c449b88a4dd02
Julien Guilmont <julien@dekalee.fr>
2017-05-05 18:43:48 +0200

    Test with tabs in vuejs

--- adback-solution-to-adblock/admin/partials/ad-back-admin-display.php
+++ adback-solution-to-adblock/admin/partials/ad-back-admin-display.php
@@ -21,101 +21,103 @@
 </p>
 <hr class="clear">
 
-<div class="tabs">
-    <a href="#global-stats" class="current"><?php _e('Global Statistics', 'ad-back'); ?></a>
-    <a href="#money-stats"><?php _e('Monetisation Statistics', 'ad-back'); ?></a>
-</div>
-
-
-<div id="global-stats" class="tab">
 <div id="ab-full-app">
     <div id="vue-app">
-        <grid>
-        <div col="3/4">
-            <datepicker></datepicker>
 
-            <div class="block-white">
-                <h4><?php _e('Blocked page view and percent', 'ad-back'); ?></h4>
-                <h7><?php _e('Blocked page view and percent - Sub', 'ad-back'); ?></h7>
-                <graph type="page-view-adblocker-percent"
-                       style="width: 95%; height: 400px; margin-bottom: 50px; position:relative;">
-                </graph>
-            </div>
+        <tabs>
+            <tab header="<?php _e('Global Statistics', 'ad-back'); ?>">
 
-            <div class="block-white">
-                <h4><?php _e('New - former adblock users', 'ad-back'); ?></h4>
-                <h7><?php _e('New - former adblock users - Sub', 'ad-back'); ?></h7>
-                <graph type="adblocker-new-old"
-                       style="width: 95%; height: 400px; margin-bottom: 50px; position:relative;">
-                </graph>
-            </div>
+                <grid>
+                    <div col="3/4">
+                        <datepicker></datepicker>
 
-            <div class="block-white">
-                <h4><?php _e('Bounce rate of adblocker users', 'ad-back'); ?></h4>
-                <graph type="bounce"
-                       style="width: 95%; height: 400px; margin-bottom: 50px; position:relative;">
-                </graph>
-            </div>
+                        <div class="block-white">
+                            <h4><?php _e('Blocked page view and percent', 'ad-back'); ?></h4>
+                            <h7><?php _e('Blocked page view and percent - Sub', 'ad-back'); ?></h7>
+                            <graph type="page-view-adblocker-percent"
+                                   style="width: 95%; height: 400px; margin-bottom: 50px; position:relative;">
+                            </graph>
+                        </div>
 
-            <div class="block-white">
-                <h4><?php _e('Browser', 'ad-back'); ?></h4>
-                <graph type="browser"
-                       style="width: 95%; height: 400px; margin-bottom: 50px; position:relative;">
-                </graph>
-            </div>
-        </div>
+                        <div class="block-white">
+                            <h4><?php _e('New - former adblock users', 'ad-back'); ?></h4>
+                            <h7><?php _e('New - former adblock users - Sub', 'ad-back'); ?></h7>
+                            <graph type="adblocker-new-old"
+                                   style="width: 95%; height: 400px; margin-bottom: 50px; position:relative;">
+                            </graph>
+                        </div>
 
-        <div col="1/4">
-            <div>
-                <progress-bar type="subscription"
-                              dashboardlink="<?php _e('https://www.adback.co/en/sites/dashboard', 'ad-back'); ?>"
-                              pricelink="<?php _e('https://www.adback.co/en/#prix', 'ad-back'); ?>"
-                              reviewlink="<?php _e('https://wordpress.org/support/plugin/adback-solution-to-adblock/reviews/', 'ad-back') ?>"
-                >
+                        <div class="block-white">
+                            <h4><?php _e('Bounce rate of adblocker users', 'ad-back'); ?></h4>
+                            <graph type="bounce"
+                                   style="width: 95%; height: 400px; margin-bottom: 50px; position:relative;">
+                            </graph>
+                        </div>
 
-                </progress-bar>
-            </div>
-        </div>
-        </grid>
+                        <div class="block-white">
+                            <h4><?php _e('Browser', 'ad-back'); ?></h4>
+                            <graph type="browser"
+                                   style="width: 95%; height: 400px; margin-bottom: 50px; position:relative;">
+                            </graph>
+                        </div>
+                    </div>
+
+                    <div col="1/4">
+                        <div>
+                            <progress-bar type="subscription"
+                                          dashboardlink="<?php _e('https://www.adback.co/en/sites/dashboard', 'ad-back'); ?>"
+                                          pricelink="<?php _e('https://www.adback.co/en/#prix', 'ad-back'); ?>"
+                                          reviewlink="<?php _e('https://wordpress.org/support/plugin/adback-solution-to-adblock/reviews/', 'ad-back') ?>"
+                            >
+
+                            </progress-bar>
+                        </div>
+                    </div>
+                </grid>
+
+
+                <center>
+                    <a href="<?php _e('https://www.adback.co/en/sites/dashboard', 'ad-back'); ?>" target="_blank"
+                       class="button button-primary button-ab"><?php esc_html_e('Discover', 'ad-back'); ?></a>
+                </center>
+            </tab>
+
+            <tab header="<?php _e('Monetisation Statistics', 'ad-back'); ?>">
+            </tab>
+
+
+        </tabs>
     </div>
 </div>
-<center>
-    <a href="<?php _e('https://www.adback.co/en/sites/dashboard', 'ad-back'); ?>" target="_blank"
-       class="button button-primary button-ab"><?php esc_html_e('Discover', 'ad-back'); ?></a>
-</center>
-</div>
-<div id="money-stats" class="tab" style="display:none;">
-
-</div>
-<script type="text/javascript">
-    window.onload = function () {
-        if (typeof adbackjs === 'object') {
-            adbackjs.init({
-                token: '<?php echo $this->getToken()->access_token; ?>',
-                url: 'https://<?php echo $this->getDomain(); ?>/api/',
-                language: '<?php echo str_replace('_', '-', get_locale()); ?>'
-            });
-        } else {
-            (function ($) {
-                $("div[data-ab-graph]").each(function () {
-                    $(this).append('<?php esc_js(printf(__('No data available, please <a href="%s">refresh domain</a>', 'ad-back'),
-                        esc_url(home_url('/wp-admin/admin.php?page=ab-refresh-domain')))); ?>');
-                });
-            })(jQuery);
-        }
-
-        (function($) {
-            $('.tabs a').click(function () {
-                if ($($(this).attr('href')).is(':visible')) {
-                    return false;
+        <script type="text/javascript">
+            window.onload = function () {
+                if (typeof adbackjs === 'object') {
+                    adbackjs.init({
+                        token: '<?php echo $this->getToken()->access_token; ?>',
+                        url: 'https://<?php echo $this->getDomain(); ?>/api/',
+                        language: '<?php echo str_replace('_', '-', get_locale()); ?>'
+                    });
+                } else {
+                    (function ($) {
+                        $("div[data-ab-graph]").each(function () {
+                            $(this).append('<?php esc_js(printf(__('No data available, please <a href="%s">refresh domain</a>', 'ad-back'),
+                                esc_url(home_url('/wp-admin/admin.php?page=ab-refresh-domain')))); ?>');
+                        });
+                    })(jQuery);
                 }
-                $(this).siblings('a.current').removeClass('current');
-                $(this).addClass('current');
-                $('.tab').hide();
-                $($(this).attr('href')).show();
-                return false;
-            });
 
-        })(jQuery);
-    }
-</script>
+                /*(function($) {
+                 $('.tabs a').click(function () {
+                 if ($($(this).attr('href')).is(':visible')) {
+                 return false;
+                 }
+                 $(this).siblings('a.current').removeClass('current');
+                 $(this).addClass('current');
+                 $('.tab').hide();
+                 $($(this).attr('href')).show();
+                 return false;
+                 });
+
+                 })(jQuery);*/
+            }
+        </script>
