bc1a7d30177221b350f07c0328bc96e4a43e86fa
Julien Guilmont <julien@dekalee.fr>
2017-05-17 18:37:44 +0200

    Update style

--- adback-solution-to-adblock/admin/partials/ad-back-admin-display.php
+++ adback-solution-to-adblock/admin/partials/ad-back-admin-display.php
@@ -20,19 +20,29 @@
             <div col="5/6">
                 <!--<tabs>
                     <tab header="<?php /*_e('Global Statistics', 'ad-back'); */?>">-->
+                        <section class="help-center">
+                            <span>
+                                <a href="javascript:history.back()" id="ab-form-start"><span class="dashicons-before dashicons-arrow-left-alt2"><?php _e('Back', 'ad-back'); ?></span></a>
+                            </span>
+                            <div style="float: right;">
+                                <a href="http://docs.adback.co/en/latest/faq/adback.html" >
+                                    <span class="dashicons-before dashicons-editor-help"><?php _e('Help center', 'ad-back');?></span>
+                                </a>
+                            </div>
+                        </section>
                         <page>
                             <h3><?php _e('Today', 'ad-back'); ?></h3>
                             <today-widget></today-widget>
 
                             <h3><?php _e('Period: Last 7 days', 'ad-back'); ?></h3>
                             <div style="margin-bottom: 10px">
-                                <browser-type style="display:inline-block"></browser-type>
+<!--                                <browser-type style="display:inline-block"></browser-type>-->
                                 <datepicker style="float:right"></datepicker>
+                                <div style="float:none;clear:both;"></div>
                             </div>
                             <section>
                                 <h4 class="header-section"><?php _e('Blocked page view and percent', 'ad-back'); ?></h4>
                                 <h7><?php _e('Blocked page view and percent - Sub', 'ad-back'); ?></h7>
-                                <hr/>
                                 <div class="section-content">
                                     <graph type="page-view-adblocker-percent"
                                            style="width: 95%; height: 400px; margin-bottom: 50px; position:relative;">
@@ -43,7 +53,6 @@
                             <section>
                                 <h4 class="header-section"><?php _e('New - former adblock users', 'ad-back'); ?></h4>
                                 <h7><?php _e('New - former adblock users - Sub', 'ad-back'); ?></h7>
-                                <hr/>
                                 <div class="section-content">
                                     <graph type="adblocker-new-old"
                                            style="width: 95%; height: 400px; margin-bottom: 50px; position:relative;">
@@ -53,7 +62,6 @@
 
                             <section>
                                 <h4 class="header-section"><?php _e('Bounce rate of adblocker users', 'ad-back'); ?></h4>
-                                <hr/>
                                 <div class="section-content">
                                     <graph type="bounce"
                                            style="width: 95%; height: 400px; margin-bottom: 50px; position:relative;">
@@ -63,7 +71,6 @@
 
                             <section>
                                 <h4 class="header-section"><?php _e('Browser', 'ad-back'); ?></h4>
-                                <hr/>
                                 <div class="section-content">
                                     <graph type="browser"
                                            style="width: 95%; height: 400px; margin-bottom: 50px; position:relative;">
@@ -73,14 +80,14 @@
 
                             <section>
                                 <hr/>
-                                <div class="section-content">
+                                <div class="section-content" style="padding-bottom:20px;">
                                     <p>
                                         <?php _e("When connecting to your AdBack dashboard, you'll discover the share of your adblocker visitors using a whitelist, the type of adblocker they use, the share of your analytics tools that are blocked (Google Analytics and Xiti), the geolocation of your adblocker users, their connection type, their operating system, the browser they use, on which device they are ; as well as data about their behavior on your site: the referral traffic, a list of your blocked pages, the bounce rate, the visit duration and the recurrence...", 'ad-back'); ?>
                                     </p>
                                     <center>
                                         <a href="<?php _e('https://www.adback.co/en/sites/dashboard', 'ad-back'); ?>"
                                            target="_blank"
-                                           class="button button-primary button-ab"><?php esc_html_e('Discover the top 10 of the blocked pages on your site and many other statistics!', 'ad-back'); ?></a>
+                                           class="button-ab"><?php esc_html_e('Discover the top 10 of the blocked pages on your site and many other statistics!', 'ad-back'); ?></a>
                                     </center>
                                 </div>
                             </section>
--- adback-solution-to-adblock/admin/partials/ad-back-admin-settings-display.php
+++ adback-solution-to-adblock/admin/partials/ad-back-admin-settings-display.php
@@ -18,11 +18,26 @@
     <div id="ab-full-app">
         <grid>
             <div col="5/6">
+                <section class="help-center">
+                    <span>
+                                <a href="javascript:history.back()" id="ab-form-start"><span class="dashicons-before dashicons-arrow-left-alt2"><?php _e('Back', 'ad-back'); ?></span></a>
+                    </span>
+                    <div style="float: right;">
+                        <a href="http://docs.adback.co/en/latest/faq/adback.html" >
+                            <span class="dashicons-before dashicons-editor-help"><?php _e('Help center', 'ad-back');?></span>
+                        </a>
+                    </div>
+                </section>
                 <div id="ab-configuration-form"></div>
-                <div class="ab-primary-setting">
-                    <h4><?php esc_html_e('Adback Account', 'ad-back'); ?></h4>
-                    <button id="ab-logout" primary m-full><?php esc_html_e('Log out', 'ad-back'); ?></button>
-                </div>
+<!--                <div class="ab-primary-setting">-->
+                    <section style="background-color:transparent;">
+                        <h4 class="header-section"><?php esc_html_e('Adback Account', 'ad-back'); ?></h4>
+                        <hr/>
+                        <div class="section-content">
+                            <button id="ab-logout" primary m-full><?php esc_html_e('Log out', 'ad-back'); ?></button>
+                        </div>
+                    </section>
+<!--                </div>-->
             </div>
             <div col="1/6">
                 <div id="adb-stats">
