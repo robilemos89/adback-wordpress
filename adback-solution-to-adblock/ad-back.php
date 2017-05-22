b7675c74695eb5509cd51f6d57d3926e316337e7
sempixel <sempixel@gmail.com>
2017-05-22 17:34:07 +0200

    Updated version

--- adback-solution-to-adblock/ad-back.php
+++ adback-solution-to-adblock/ad-back.php
@@ -16,7 +16,7 @@
  * Plugin Name:       AdBack solution to adblock
  * Plugin URI:        adback.co
  * Description:       With AdBack, access analytics about adblocker users, address them personalized messages, propose alternative solutions to advertising (video, survey).
- * Version:           1.2.6
+ * Version:           2.0.0
  * Author:            AdBack
  * Author URI:        https://www.adback.co
  * License:           GPL-2.0+
