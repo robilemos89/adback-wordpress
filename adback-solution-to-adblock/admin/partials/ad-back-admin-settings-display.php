a88b9b2317bf69f207e25b2330ddd376f0db4fda
Victor Balssa <victor@adback.co>
2018-01-23 14:17:44 +0100

    logout btn css update

--- adback-solution-to-adblock/admin/partials/ad-back-admin-settings-display.php
+++ adback-solution-to-adblock/admin/partials/ad-back-admin-settings-display.php
@@ -20,22 +20,22 @@
             <div col="5/6">
 
                 <div id="ab-configuration-form"></div>
-<!--                <div class="ab-primary-setting">-->
-                    <section style="background-color:transparent;">
-                        <h4 class="header-section"><?php esc_html_e('Get more statistics', 'adback-solution-to-adblock'); ?></h4>
-                        <hr/>
-                        <div class="section-content">
-                            <button id="ab-website" primary m-full><?php esc_html_e('Redirect me to my AdBack dashboard', 'adback-solution-to-adblock'); ?></button>
-                        </div>
-                    </section>
-                    <section style="background-color:transparent;">
-                        <h4 class="header-section"><?php esc_html_e('Adback Account', 'adback-solution-to-adblock'); ?></h4>
-                        <hr/>
-                        <div class="section-content">
-                            <button id="ab-logout" primary m-full><?php esc_html_e('Log out', 'adback-solution-to-adblock'); ?></button>
-                        </div>
-                    </section>
-<!--                </div>-->
+                <section style="background-color:transparent;">
+                    <h4 class="header-section"><?php esc_html_e('Get more statistics', 'adback-solution-to-adblock'); ?></h4>
+                    <hr/>
+                    <div class="section-content">
+                        <button id="ab-website" class="btn-generic-save" primary
+                                m-full><?php esc_html_e('Redirect me to my AdBack dashboard', 'adback-solution-to-adblock'); ?></button>
+                    </div>
+                </section>
+                <section style="background-color:transparent;">
+                    <h4 class="header-section"><?php esc_html_e('Adback Account', 'adback-solution-to-adblock'); ?></h4>
+                    <hr/>
+                    <div class="section-content">
+                        <button id="ab-logout" class="btn-generic-save" primary
+                                m-full><?php esc_html_e('Log out', 'adback-solution-to-adblock'); ?></button>
+                    </div>
+                </section>
             </div>
             <div col="1/6">
                 <div id="adb-sidebar-standalone"
@@ -64,7 +64,7 @@
             $("#ab-website").on('click', function () {
                 var locale = "<?php echo explode('_', get_locale())[0]; ?>";
                 var email = "<?php echo get_bloginfo('admin_email') ?>";
-                window.location.href = 'https://www.adback.co/'+locale+'/login?_login_email='+email;
+                window.location.href = 'https://www.adback.co/' + locale + '/login?_login_email=' + email;
             });
         });
     })(jQuery);
